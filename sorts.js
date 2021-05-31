4530d764cba7b51685e00c73420bbf4e8f6c19fb
Kevyn Higbee <35093449+Myute@users.noreply.github.com>
2021-05-31 15:42:32 -0700

    bubblesort

--- sorts.js
+++ sorts.js
@@ -33,17 +33,27 @@ function shuffleArray(a) {
 }
 
 function swap(arr, a, b) {
-    var tmp = arr[a]
-    arr[a] = arr[b]
-    arr[b] = tmp
+    var tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
 }
 
 function bubblesort(a) {
     var i, j;
+    var len = a.length
 
+    for (i = 0; i < len - 1; i++) {
+        for (j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1)
+            }
+        }
+    }
 }
 
 function selectionsort(a) {
+    let len = a.length;
+
 
 }
 
